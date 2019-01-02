313e8325af60978fc1e72e257378b89b9558f457
gmahesh007babu@gmail.com <gmahesh007babu@gmail.com>
2019-01-03 03:01:34 +0530

    update readme and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Event.create(name: "Mahesh", duration: 20, start_time: DateTime.now)
+Event.create(name: "Babu", duration: 20, start_time: DateTime.now-2.hours)
+Event.create(name: "ROvin", duration: 20, start_time: DateTime.now-3.hours)
+Event.create(name: "Sandeep", duration: 40, start_time: DateTime.now-3.hours)
+Event.create(name: "Chirag", duration: 40, start_time: DateTime.now-4.hours)
